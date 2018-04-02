b157f3260ecf1796557f9cc8dab42f34cdb9fab7
Luís Silva <lm2s@gmx.com>
2018-04-02 12:13:56 +0100

    Initial Commit

---
+++ NanoNetworkStack/NanoNetworkStack.h
@@ -0,0 +1,19 @@
+//
+//  NanoNetworkStack.h
+//  NanoNetworkStack
+//
+//  Created by Luís Silva on 02/04/2018.
+//  Copyright © 2018 Chipp'd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NanoNetworkStack.
+FOUNDATION_EXPORT double NanoNetworkStackVersionNumber;
+
+//! Project version string for NanoNetworkStack.
+FOUNDATION_EXPORT const unsigned char NanoNetworkStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NanoNetworkStack/PublicHeader.h>
+
+
